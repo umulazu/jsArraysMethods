c3b4225bb78149d3844596ced6a714fe3e1868de
ANDREI-PC\umula <umulazu@yandex.ru>
2018-07-01 14:48:11 +0300

    DOM + map + filter

--- main.js
+++ main.js
@@ -60,8 +60,14 @@ const sortedInventorsByLived = inventors.sort( (a, b) => {
 });
 console.table(sortedInventorsByLived);
 
+
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const category = document.querySelector('.mw-category');
+const links = Array.from(category.querySelectorAll('a'));
+const textLinks = links.map( a => a.textContent);
+const deLinks = textLinks.filter( textLink => textLink.includes('de'));
+
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 // 8. Reduce Exercise
